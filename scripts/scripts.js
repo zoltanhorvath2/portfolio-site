9344be968fbb1c960ef20abac5489dbf3c8ac59f
Zoltan Horvath <zoltan.horvath@proxybit.hu>
2022-02-01 16:38:02 +0100

    add navbar dark onscroll

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
-console.log('scripts are wired up')
+// make navbar dark onscroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
